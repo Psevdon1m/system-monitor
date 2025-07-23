1c7c38863ab4e9bacc2214395ea7b5f6faec8b6c
Psevdon1m <Valentine.oleynik@gmail.com>
2025-07-23 19:53:57 +0300

    add push alerts on overload

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -6,16 +6,9 @@ const os = osu.os;
 
 //variables
 
-const cpu_overload = 10;
-let lastNotifTime = new Date().getTime();
+const cpu_overload = 20;
+const alertFrequency = 1; //minutes
 
-notifyUser({
-  title: "CPU Overload",
-  body: `CPU is over ${cpu_overload}%`,
-  icon: path.join(__dirname, "img", "icon.png"),
-});
-
-// run every 2s
 function getAndSetupCpuUsage() {
   cpu.usage().then((i) => {
     document.getElementById("cpu-usage").innerText = Number(i).toFixed(2) + "%";
@@ -28,7 +21,18 @@ function getAndSetupCpuUsage() {
     } else {
       document.getElementById("cpu-progress").style.background = "#30c88b";
     }
+    //check overload
+
+    if (i >= cpu_overload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: "CPU Overload",
+        body: `CPU is over ${cpu_overload}%`,
+        icon: path.join(__dirname, "img", "icon.png"),
+      });
+      localStorage.setItem("lastNotifyTime", new Date().getTime());
+    }
   });
+
   cpu.free().then((i) => {
     document.getElementById("cpu-free").innerText = Number(i).toFixed(2) + "%";
   });
@@ -37,6 +41,7 @@ function getAndSetupCpuUsage() {
   document.getElementById("sys-uptime").innerText = secondsToDHMS(os.uptime());
 }
 getAndSetupCpuUsage();
+// run every 2s
 setInterval(() => {
   //CPU Usage
   getAndSetupCpuUsage();
@@ -73,4 +78,30 @@ function secondsToDHMS(sec) {
 
 function notifyUser(options) {
   new Notification(options.title, options);
+}
+
+//check when last notif has been sent
+function runNotify(frequency) {
+  if (localStorage.getItem("lastNotifyTime") === null) {
+    console.log("no prev saved time");
+
+    //store timestamp
+    localStorage.setItem("lastNotifyTime", new Date().getTime());
+    return true;
+  }
+
+  const notifyTime = Math.floor(
+    Number(localStorage.getItem("lastNotifyTime")) / 1000
+  );
+  const now = Math.floor(new Date().getTime() / 1000);
+  const diff = now - notifyTime;
+  console.log({ notifyTime, now, diff });
+
+  const minutesPassed = Math.ceil(diff / 60);
+  console.log({ minutesPassed, frequency });
+
+  if (minutesPassed > frequency) {
+    return true;
+  }
+  return false;
 }
